0727431a2561c90852941c3d1ef3ee5a8ea2b508
jx0906 <sandy_hong@live.com>
2023-10-01 21:58:15 +0800

    UI update

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ class game {
     getTotalGameCards() {
         return this.totalCards;
     }
-
 }
 
 var game1 = new game(1, 6);
@@ -34,9 +33,14 @@ class card {
     this.value = value;
     this.status= status;
     this.selection = selection;
+    this.label = "label"; //optional argument?
     }
 
-    getCardVal() {
+    getCardLabel() {
+        return this.label;
+    }
+
+    getCardValue() {
         return this.value;
     }
 
@@ -44,15 +48,19 @@ class card {
         return this.status;
     }
 
-    getCardSel() {
+    getCardSelection() {
         return this.selection;
     }
     
-    setCardVal(val) {
+    setCardLabel(newLabel) {
+        this.label = newLabel;
+    }
+
+    setCardValue(val) {
         this.value = val;
     }
 
-    setCardSel(playerNotSel) {
+    setCardSelection(playerNotSel) {
         this.selection = playerNotSel;
     }
 
@@ -61,17 +69,17 @@ class card {
         this.selection = playerNotSel;
     }
 }
-
 // class Player - if i want to do scoreboard
 
 // state variables
 
 let cardArray = [];
 
+//always try to set smth for the keys in the object
 let gameState = {
     level: null,
     firstCardSel: null,
-    timeLeft: 60000,
+    timeLeft: 60,
     cardsLeft: 0
 }
 
@@ -86,8 +94,8 @@ const gameCards = document.getElementById('game-cards');
 // event listeners
 
 playButton.addEventListener('click', initialize);
-gCards.addEventListener('click', handleMove);
-// to add event listener for time's up (no user action)
+gameCards.addEventListener('click', handleMove);
+// to add event listener for time's up (no user action)?
 
 // functions
 
@@ -97,45 +105,64 @@ function initialize() {
     gameState.level = game1.getGameLevel();
     gameState.cardsLeft = game1.getTotalGameCards();
     createCards(game1.getTotalGameCards());
-    setTimeout(countdown(), 60000);
+    updateCountdown(); 
     rStartGame();
 }
 
-    // need to assign: gameSetting.level = 1 and gameSetting.totalGameCards = 6
-    // createCards()
-    // set gameState.cardsLeft = gameSetting.totalGameCards
-    // render? -
-        // update play-button from "Start Game" to "Replay!";
-        // gameStatus = "your time starts now!"
-        // clear gameArea, timer, selection-display
-        // display new set of cards and timer
-
-
 function createCards(totalCardsToCreate) {
+    while (gameCards.hasChildNodes()) {
+        gameCards.
+        removeChild(gameCards.firstChild);
+    }
+
     for (i = 0; i < (totalCardsToCreate /2); i++) {
         // get a random item from an array
         // get random index value
         const randomIndex = Math.floor(Math.random() * cardCombi.length);
         let combiToCreate = cardCombi[randomIndex];
-        console.log(combiToCreate) // returns "object array" ie, cardCombi[randomIndex] is an array within combiToCreate array
-        var cardA = new card(combiToCreate[0], "active", "playerNotSel");
-        var cardB = new card(combiToCreate[1], "active", "playerNotSel");
+        // console.log(combiToCreate); for debugging, to confirm what we are getting is a set of numbers
+        let cardA = new card(combiToCreate[0], "active", "playerNotSel");
+        let cardB = new card(combiToCreate[1], "active", "playerNotSel");
         cardArray.push(cardA, cardB); //doesn't replace, just appends, so command would work in this loop and not reset cardArray each loop
     }
-        console.log(cardArray);
+        // console.log(cardArray); for debugging to confirm cardArray is rightly established
+    for (j = 0; j < cardArray.length; j++) {
+        cardArray[j].setCardLabel("card " + j); //want to do this so i can assign the created DOM element with the same label to facilitate
+        // subsequent needs to check on the card class properties (eg, getClassSelection())
+    }
+    // console.log(cardArray[1]); for debugging to confirm the cardLabel is rightly assigned
     }
 
+// rmb to write codes to update Model, not DOM (that's render)
+function handleMove(evt) {
+    // console.log(evt.target); //gives DOM value so we need to call on the its ID to link it back to the cardArraylet clickedCardID = evt.target.id; // returns "card 1"
+    // (based on the way I had created the cards)
 
-// createCards
-    // set cardsToCreate = gameSetting.totalGameCards / 2
-    // Loop x no. of cardsToCreate times:
-        // randomly choose one combi from cardCombi
-            // var cardA = new card(cardCombi[0], "active", "playerNotSel");
-            // var cardB = new card(cardCombi[1], "active", "playerNotSel");
-            // cardArray.push(cardA, cardB);
+    // "id" is a DOM property so you  have to be on the DOM element object to use it.
+    let idx = parseInt(evt.target.id.replace('card ', ''));
+    // console.log(idx);
+    let clickedCard = cardArray[idx];
+    console.log(clickedCard); // returns an object
+    //clickedCard.getCardSelection() doesn't work. is it cos clickedCard is not governed by the class properties as an object?
 
-// handleMove (rmb to write codes to update Model, NOT DOM!!!!)
-// set clickedCard = evt.target...
+    let clickedCardSel = clickedCard.getCardSelection();
+    console.log(clickedCardSel);
+
+    if (clickedCardSel === "playerSel") {
+        clickedCard.setCardSelection("playerNotSel");
+        gameState.firstCardSel = null;
+        renderCards(evt);
+        return;
+    }
+    clickedCard.setCardSelection("playerSel");
+    // console.log(clickedCard);
+    // console.log(gameState.firstCardSel);
+    gameState.firstCardSel = clickedCard;
+    console.log(gameState.firstCardSel);
+    renderCards(evt);
+    return;
+    }
+
 // to check card.selection of clicked card (evt.target)
     // if "playerSel", update to "playerNotSel" (make sure renderView also updates accordingly, ie, selectionDisplay.textContent = "? + ? = 10 " )
     // if "playerNotSel", update to "playerSel"
@@ -161,47 +188,66 @@ function createCards(totalCardsToCreate) {
     // - if gameState.cardsLeft > 0, continue
 
 // updateGame, refresh game for next level 
-// createCards()
-
-function rStartGame() {
-        playButton.textContent = 'Replay!';
-        renderCards();
-        gameStatus.textContent = 'Your time starts now!';
-}
-
-function renderCards() {
-    for (let i = 0; i < cardArray.length; i++) {
-        //this will lead to the same effect as having </div><div> in the .game-board HTML elem to facilitate css styling, js functions
-        const gCard = document.createElement('ul');
-        gCard.setAttribute("id",i);
-        gCard.setAttribute("class","cards");
-        gCard.innerText = cardArray[i].value;
-        // Append created card to the pile of gameCards so that the css styling can be applied
-        gameCards.appendChild(gCard);
-    }
-};
 
 
 // Update the count down every 1 second
-function countdown() {
+function updateCountdown() {
+    // Set end time to be 1 min later (ie, 60000 miliseconds)
+    // can use new Date.getTime() if you want to be very accurate about tmekeeping cos 1000 miliseconds != exactly 1sec. its more of 1.03 s.
+    // var now = new Date().getTime(); var targetTime = 1 minute later (need to find the exact code)
 
-  // Set end time to be 1 min later (ie, 60000 miliseconds)
-  var now = new Date();
-  var targetTime = now + 60000;
+    const seconds = gameState.timeLeft % 60; // how to get rid of the 0 s which appears at the start when gameState.timeLeft = 60 and 60%60  =0 
+    timerDisplay.textContent = `${seconds} s`;
+    const timer = setInterval(updateCountdown, 1000); // use setInterval to call the updateCountdown function every second (1000 milliseconds).  
+    // If the count down is finished, 
+    if (gameState.timeLeft === 0) {
+      clearInterval(timer);
+      timerDisplay.textContent = "TIME'S UP!";
+      return;
+    }
+    gameState.timeLeft--;
+  };
 
-  // Find the distance between now and the count down date
-  var distance = targetTime - now;
+// for game start and game refresh (ie, new level)
+function rStartGame() {
+    playButton.textContent = 'Replay!';
+    rCreateCards();
+    gameStatus.textContent = 'Your time starts now!'
+    setInterval(newMessage, 500);
+}
 
-  // Time calculations for days, hours, minutes and seconds
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  timerDisplay.innerHTML = seconds + "s ";
-
-  //how to update gameState.timeleft?
-
-  // If the count down is finished, 
-  if (distance < 0) {
-    clearInterval(x);
-    timerDisplay.innerHTML = "TIME'S UP!";
-    gameState.timeLeft = 0;
-  }
+function newMessage() {
+    gameStatus.textContent = '? + ? = 10'
 };
+
+function rCreateCards() {
+    for (let i = 0; i < cardArray.length; i++) {
+        //this will lead to the same effect as having </div><div> in the .game-board HTML elem to facilitate css styling, js functions
+        let gCard = document.createElement('div');
+        gCard.classList.add("cards", cardArray[i].getCardStatus(), cardArray[i].getCardSelection())
+        gCard.setAttribute("id",cardArray[i].getCardLabel());
+        gCard.innerText = cardArray[i].getCardValue();
+        // Append created card to the pile of gameCards so that the css styling can be applied
+        gameCards.appendChild(gCard);
+    };
+}
+
+function renderCards(event) {
+    const selCard = document.getElementById(event.target.id);
+    const hasplayerSelClass = selCard.classList.contains('playerSel');
+    if (hasplayerSelClass) {
+        selCard.classList.replace('playerSel', 'playerNotSel');
+        gameStatus.textContent = '? + ? = 10'
+    }
+    selCard.classList.replace('playerNotSel', 'playerSel');
+    console.log(selCard.textContent);
+    gameStatus.textContent = `${selCard.textContent} + ? = 10`;
+};
+
+// Reference: Fisher-Yates shuffle algorithm
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // Swap elements at i and j
+  }
+}
